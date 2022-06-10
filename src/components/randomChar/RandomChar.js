3ae9aa8429e0a9f03ad2c2d81c0ee6078299016c
mrdom2 <pedroasd35@gmail.com>
2022-06-10 23:16:26 +0300

    1st

---
+++ src/components/randomChar/RandomChar.js
@@ -0,0 +1,74 @@
+import { Component } from 'react';
+import MarvelService from '../../services/MarvelService';
+
+import './randomChar.scss';
+import mjolnir from '../../resources/img/mjolnir.png';
+
+
+
+class RandomChar extends Component {
+    constructor(props) {
+        super(props);
+        this.updateChar();
+    }
+
+    state = {
+        char: {}
+    }
+
+    onCharLoaded = (char) => {
+        this.setState({ char });
+    }
+
+    marvelService = new MarvelService();
+
+    updateChar = () => {
+        const randomId = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+
+        this.marvelService
+            .getSingleCharacter(randomId)
+            .then(this.onCharLoaded);
+    }
+
+
+    render() {
+        const { char: { name, desciption, thumbnail, homepage, wiki } } = this.state;
+
+        return (
+            <div className="randomchar">
+                <div className="randomchar__block">
+                    <img src={thumbnail} alt="Random character" className="randomchar__img" />
+                    <div className="randomchar__info">
+                        <p className="randomchar__name">{name}</p>
+                        <p className="randomchar__descr">
+                            {desciption}
+                        </p>
+                        <div className="randomchar__btns">
+                            <a href={homepage} target="_blank" className="button button__main">
+                                <div className="inner">homepage</div>
+                            </a>
+                            <a href={wiki} target="_blank" className="button button__secondary">
+                                <div className="inner">Wiki</div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="randomchar__static">
+                    <p className="randomchar__title">
+                        Random character for today!<br />
+                        Do you want to get to know him better?
+                    </p>
+                    <p className="randomchar__title">
+                        Or choose another one
+                    </p>
+                    <button className="button button__main">
+                        <div className="inner">try it</div>
+                    </button>
+                    <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default RandomChar;
---
+++ src/services/MarvelService.js
@@ -0,0 +1,37 @@
+
+
+class MarvelService {
+    _apiBase = "https://gateway.marvel.com:443/v1/public/";
+    _apiKey = "apikey=9ad839a9c88d121575614a5606a17237";
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Couldn't fetch ${url}, status: ${res.status}`);
+        }
+        return await res.json();
+    }
+
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getSingleCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (myChar) => {
+        return {
+            name: myChar.name,
+            decription: myChar.desciption,
+            thumbnail: `${myChar.thumbnail.path}.${myChar.thumbnail.extension}`,
+            homepage: myChar.urls[0].url,
+            wiki: myChar.urls[1].url
+        }
+    }
+}
+
+export default MarvelService;
