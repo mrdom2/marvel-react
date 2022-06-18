0aca531d8c5d7a3ca6e832dbcc4848c1fc2b55ba
mrdom2 <pedroasd35@gmail.com>
2022-06-18 10:55:48 +0300

    ref

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -19,7 +19,7 @@ class CharList extends Component {
     marvelService = new MarvelService();
 
     componentDidMount() {
-        this.onRequest()
+        this.onRequest();
     }
 
     onRequest = (offset) => {
@@ -55,8 +55,22 @@ class CharList extends Component {
         })
     }
 
+    itemRefs = [];
+
+    setRef = (ref) => {
+        this.itemRefs.push(ref);
+    }
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
+    }
+
+    // Этот метод создан для оптимизации, 
+    // чтобы не помещать такую конструкцию в метод render
     renderItems(arr) {
-        const items = arr.map((item) => {
+        const items = arr.map((item, i) => {
             let imgStyle = { 'objectFit': 'cover' };
             if (item.thumbnail.includes("image_not_available")) {
                 imgStyle = { 'objectFit': 'unset' };
@@ -65,14 +79,25 @@ class CharList extends Component {
             return (
                 <li
                     className="char__item"
+                    tabIndex={0}
+                    ref={this.setRef}
                     key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}>
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i);
+                    }}
+                    onKeyPress={(e) => {
+                        if (e.key === ' ' || e.key === "Enter") {
+                            this.props.onCharSelected(item.id);
+                            this.focusOnItem(i);
+                        }
+                    }}>
                     <img src={item.thumbnail} alt={item.name} style={imgStyle} />
                     <div className="char__name">{item.name}</div>
                 </li>
             )
         });
-
+        // А эта конструкция вынесена для центровки спиннера/ошибки
         return (
             <ul className="char__grid">
                 {items}
@@ -95,10 +120,10 @@ class CharList extends Component {
                 {spinner}
                 {content}
                 <button
-                    onClick={() => this.onRequest(offset)}
                     className="button button__main button__long"
                     disabled={newItemLoading}
-                    style={{ 'display': charEnded ? 'none' : 'block' }}>
+                    style={{ 'display': charEnded ? 'none' : 'block' }}
+                    onClick={() => this.onRequest(offset)}>
                     <div className="inner">load more</div>
                 </button>
             </div>
